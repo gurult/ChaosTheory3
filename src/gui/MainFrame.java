6b0772676b6348cbdcb38eb6a9d0891fbbf4889a
Gurult <ruslliz666@gmail.com>
2014-11-15 13:09:51 +0200

    padarytas šioks toks gui
    padarytas gui, kur pasirenkami medžio parametrai

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,10 +1,14 @@
 package gui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame{
+	
 	// fields
+	private TreeSetingsPanel treePanel;
 	
 	//Constructor
 	
@@ -18,5 +22,14 @@ public class MainFrame extends JFrame{
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		//layout mannager
+		
+		setLayout(new BorderLayout());
+		
+		//intint setengs pane;
+		
+		treePanel = new TreeSetingsPanel();
+		add(treePanel, BorderLayout.CENTER);
 	}
 }
---
+++ src/gui/TreeSetingsPanel.java
@@ -0,0 +1,235 @@
+package gui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+
+@SuppressWarnings("serial")
+public class TreeSetingsPanel extends JPanel {
+	
+	private static final String FORMAT = "0.00";
+	private static final String FORMAT1 = "000";
+	
+	//Pagrindiniai label
+	
+	JLabel firstTreeConfigLabel = new JLabel("Pirmos ðakos nustatymai :");
+	JLabel secondTreeConfigLabel  = new JLabel("Antros ðakos nustatymai :");
+	JLabel thirdTreeConfigLabel  = new JLabel("Treèios ðakos nustatymai : ");
+	JLabel fourthTreeConfigLabel  = new JLabel("Ketvirtos ðakos nustatymai : ");
+	
+	
+	//Spinners
+	JLabel firstBranchResize = new JLabel("Maþinimas : ");
+	JLabel secondBranchResize = new JLabel("Maþinimas : ");
+	JLabel thirdBranchResize = new JLabel("Maþinimas : ");
+	JLabel fourthBranchResize = new JLabel("Maþinimas : ");
+	
+	JSpinner firstSpinner;
+	JSpinner secondSpinner;
+	JSpinner thirdSpinner;
+	JSpinner fourthSpinner;
+	
+	// 4 label ir 4 textfield
+	
+	JLabel angle1Label = new JLabel("Posukis : ");
+	JLabel angle2Label = new JLabel("Posukis : ");
+	JLabel angle3Label = new JLabel("Posukis : ");
+	JLabel angle4Label = new JLabel("Posukis : ");
+	
+	JSpinner firstSpinnerDeg;
+	JSpinner secondSpinnerDeg;
+	JSpinner thirdSpinnerDeg;
+	JSpinner fourthSpinnerDeg;
+	
+	// button
+	
+	JLabel createTreeLabel = new JLabel("sukurti medi");
+	JButton createTreeButton = new JButton("Create");
+	
+	public TreeSetingsPanel() {
+		setVisible(true);
+		initSpiners();
+		initLayout();
+		
+		createTreeButton.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(firstSpinner.getValue());
+				double x= (Double)firstSpinner.getValue();
+				
+			}});
+	}
+	
+	private void initLayout() {
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		// inicilizavome konstantas
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 0;
+		c.weighty = 0;
+
+		// pirmos ðakos parametrai
+		
+		add(firstTreeConfigLabel, c);
+		
+		c.gridx = 0;
+		c.gridy++;
+		c.weightx = 0;
+		c.weighty = 0;
+		
+
+		add(firstBranchResize, c);
+		
+		c.gridx++;
+		c.weightx = 0;
+		c.weighty = 0;
+
+		add(firstSpinner, c);
+		
+		c.gridx = 0;
+		c.gridy++;
+		add(angle1Label, c);
+		
+		c.gridx++;
+		add(firstSpinnerDeg, c);
+		
+		// 2 ðakos parametrai
+		
+		c.gridx = 0;
+		c.gridy ++;
+		
+		add(secondTreeConfigLabel, c);
+		
+		c.gridx = 0;
+		c.gridy ++;
+		
+		c.gridx = 0;
+		c.gridy++;
+		c.weightx = 0;
+		c.weighty = 0;
+		
+
+		add(secondBranchResize, c);
+		
+		c.gridx++;
+		c.weightx = 0;
+		c.weighty = 0;
+
+		add(secondSpinner, c);
+		
+		c.gridx = 0;
+		c.gridy++;
+		add(angle2Label, c);
+		
+		c.gridx++;
+		add(secondSpinnerDeg, c);
+		
+		
+		// 3 ðakos parametrai
+		
+		c.gridx = 0;
+		c.gridy ++;
+		
+		add(thirdTreeConfigLabel, c);
+		
+		c.gridx = 0;
+		c.gridy ++;
+		
+		c.gridx = 0;
+		c.gridy++;
+		c.weightx = 0;
+		c.weighty = 0;
+		
+
+		add(thirdBranchResize, c);
+		
+		c.gridx++;
+		c.weightx = 0;
+		c.weighty = 0;
+
+		add(thirdSpinner, c);
+		
+		c.gridx = 0;
+		c.gridy++;
+		add(angle3Label, c);
+		
+		c.gridx++;
+		add(thirdSpinnerDeg, c);
+		
+		// 4 ðakos parametrai
+		
+		c.gridx = 0;
+		c.gridy ++;
+		
+		add(fourthTreeConfigLabel, c);
+		
+		c.gridx = 0;
+		c.gridy ++;
+		
+		c.gridx = 0;
+		c.gridy++;
+		c.weightx = 0;
+		c.weighty = 0;
+		
+
+		add(fourthBranchResize, c);
+		
+		c.gridx++;
+		c.weightx = 0;
+		c.weighty = 0;
+
+		add(fourthSpinner, c);
+		
+		c.gridx = 0;
+		c.gridy++;
+		add(angle4Label, c);
+		
+		c.gridx++;
+		add(fourthSpinnerDeg, c);
+		
+		// add button
+		
+		c.gridx = 0;
+		c.gridy ++;
+		
+		add(createTreeLabel, c);
+
+		c.gridx++;
+		add(createTreeButton, c);
+	}
+	
+	private void initSpiners() {
+				
+		firstSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 1, 0.01));
+		secondSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 1, 0.01));
+		thirdSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 1, 0.01));
+		fourthSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 1, 0.01));
+		
+		firstSpinner.setEditor(new JSpinner.NumberEditor(firstSpinner, FORMAT));
+		secondSpinner.setEditor(new JSpinner.NumberEditor(secondSpinner, FORMAT));
+		thirdSpinner.setEditor(new JSpinner.NumberEditor(thirdSpinner, FORMAT));
+		fourthSpinner.setEditor(new JSpinner.NumberEditor(fourthSpinner, FORMAT));
+		
+		//-----------------------------------------
+		firstSpinnerDeg = new JSpinner(new SpinnerNumberModel(0, 0, 180, 1));
+		secondSpinnerDeg = new JSpinner(new SpinnerNumberModel(0, 0, 180, 1));
+		thirdSpinnerDeg = new JSpinner(new SpinnerNumberModel(0, 0, 180, 1));
+		fourthSpinnerDeg = new JSpinner(new SpinnerNumberModel(0, 0, 180, 1));
+		
+		firstSpinnerDeg.setEditor(new JSpinner.NumberEditor(firstSpinnerDeg, FORMAT1));
+		secondSpinnerDeg.setEditor(new JSpinner.NumberEditor(secondSpinnerDeg, FORMAT1));
+		thirdSpinnerDeg.setEditor(new JSpinner.NumberEditor(thirdSpinnerDeg, FORMAT1));
+		fourthSpinnerDeg.setEditor(new JSpinner.NumberEditor(fourthSpinnerDeg, FORMAT1));
+	}
+}
