6a6a009da5f4e00a3b2db205c67f2856d54150a8
Gurult <ruslliz666@gmail.com>
2014-12-03 00:51:20 +0200

    atsiskaitimui

--- src/gui/TreeDrawingPanel.java
+++ src/gui/TreeDrawingPanel.java
@@ -24,29 +24,21 @@ public class TreeDrawingPanel extends JPanel {
 	private BranchParams[] bp;
 	private Point2D temp = new Point2D.Double();
 
-	private int wind = 0;
-	private int wind2 = 45;
-	private int counter = 0;
-
 	private List<MyLine> firstList = new ArrayList<MyLine>();
 	private List<MyLine> scondList = new ArrayList<MyLine>();
 	private List<MyLine> thirdList;
 
-	
-	private List<MyLine> fourth = new ArrayList<MyLine>();
-	private List<MyLine> fifth = new ArrayList<MyLine>();
-	private List<MyLine> sixth;
+	private int counter = 0;
+	private int wind = 0;
+	private boolean kaire = true;
+	private boolean desne = false;
+
 	// /------------------------
 	public TreeDrawingPanel() {
 
 		// inicilizuojame savo pirmaji taðka
 
-		firstList.add(new MyLine(250, 350, 250, 250));
-		scondList.add(new MyLine(250, 350, 250, 250));
-		
-		fourth.add(new MyLine(250, 350, 250, 250));
-		fifth.add (new MyLine(250, 350, 250, 250));
-		
+
 		setLayout(new BorderLayout());
 		add(nextIterationButton, BorderLayout.SOUTH);
 
@@ -55,182 +47,126 @@ public class TreeDrawingPanel extends JPanel {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
-				test2();
-				test();
-				repaint();
-			}
+
+				
+					if ((kaire) && wind < 5) {
+						// System.out.println("suveikia");
+						wind++;
+					} else {
+						desne = true;
+						kaire = false;
+					}
+
+					if ((desne) && wind > -5) {
+						wind--;
+					} else {
+						kaire = true;
+						desne = false;
+
+					}
+					System.out.println(wind);
+					System.out.println("counter" + counter);
+					test(wind);
+					repaint();
+				}
+			
+			
 		});
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		if(counter < 4 ) 
-		{
+		if (scondList != null) {
 			for (int i = 0; i < scondList.size(); i++) {
 				g2.draw(new Line2D.Double(scondList.get(i).a, scondList.get(i).b));
 			}
-		}else {
-			System.out.println("piesia");
-			for (int i = 0; i < fifth.size(); i++) {
-				g2.draw(new Line2D.Double(fifth.get(i).a, fifth.get(i).b));
-		}}
-		
+		System.out.println("antro saraso dydis" + scondList.size());
+		scondList.clear();
+		firstList.clear();
+		}
 	}
-	
 
 	public void setBps(BranchParams[] branchParams) {
 		this.bp = branchParams;
+		bp[0].printInfo();
+
+		// repaint();
 	}
 
 	// darbas su pirmu medziu
 
-	private void w1(Point2D a) {
-		AffineTransformations.affineTransformPos(temp, a, bp[0].angle, bp[0].scale);
+	private void w1(Point2D a, int wind) {
+		AffineTransformations.affineTransformPos(temp, a, bp[0].angle - wind, bp[0].scale);
 	}
 
-	private void w2(Point2D a) {
-		AffineTransformations.affineTransformPos(temp, a, bp[1].angle, bp[1].scale);
+	private void w2(Point2D a, int wind) {
+		AffineTransformations.affineTransformPos(temp, a, bp[1].angle - wind, bp[1].scale);
 	}
 
-	private void w3(Point2D a) {
-		AffineTransformations.affineTransformPos(temp, a, bp[2].angle, bp[2].scale);
+	private void w3(Point2D a, int wind) {
+		AffineTransformations.affineTransformPos(temp, a, bp[2].angle - wind, bp[2].scale);
 	}
 
-	private void w4(Point2D a) {
-		AffineTransformations.affineTransformPos(temp, a, bp[3].angle, bp[3].scale);
-	}
-	
-	
-	//---------------------------------------------------------
-	// antras medis
-	
-	private void w21(Point2D a) {
-		AffineTransformations.affineTransformPos(temp, a, bp[0].angle - wind2, bp[0].scale);
+	private void w4(Point2D a, int wind) {
+		AffineTransformations.affineTransformPos(temp, a, bp[3].angle - wind, bp[3].scale);
 	}
 
-	private void w22(Point2D a) {
-		AffineTransformations.affineTransformPos(temp, a, bp[1].angle - wind2, bp[1].scale);
-	}
+	// ---------------------------------------------------------
 
-	private void w23(Point2D a) {
-		AffineTransformations.affineTransformPos(temp, a, bp[2].angle - wind2, bp[2].scale);
-	}
+	// ---------------------------------------------------------
 
-	private void w24(Point2D a) {
-		AffineTransformations.affineTransformPos(temp, a, bp[3].angle - wind2, bp[3].scale);
-	}
-	//-------------------------------------------------------
-	
-	private void ww21(MyLine a) {
+	private void ww1(MyLine a, int wind) {
 
 		MyLine tempLine = new MyLine(a.a, a.b);
 
-		w21(tempLine.a);
-		w21(tempLine.b);
-
-		fifth.add(new MyLine(tempLine.a, tempLine.b));
-		sixth.add(new MyLine(tempLine.a, tempLine.b));
-	}
-
-	private void ww22(MyLine a) {
-
-		MyLine tempLine = new MyLine(a.a, a.b);
-
-		w22(tempLine.a);
-		w22(tempLine.b);
-
-		fifth.add(new MyLine(tempLine.a, tempLine.b));
-		sixth.add(new MyLine(tempLine.a, tempLine.b));
-	}
-
-	private void ww23(MyLine a) {
-
-		MyLine tempLine = new MyLine(a.a, a.b);
-
-		w23(tempLine.a);
-		w23(tempLine.b);
-
-		fifth.add(new MyLine(tempLine.a, tempLine.b));
-		sixth.add(new MyLine(tempLine.a, tempLine.b));
-	}
-
-	private void ww24(MyLine a) {
-
-		MyLine tempLine = new MyLine(a.a, a.b);
-
-		w24(tempLine.a);
-		w24(tempLine.b);
-
-		fifth.add(new MyLine(tempLine.a, tempLine.b));
-		sixth.add(new MyLine(tempLine.a, tempLine.b));
-
-	}
-
-	
-	//---------------------------------------------------------
-
-	private void ww1(MyLine a) {
-
-		MyLine tempLine = new MyLine(a.a, a.b);
-
-		w1(tempLine.a);
-		w1(tempLine.b);
+		w1(tempLine.a, wind);
+		w1(tempLine.b, wind);
 
 		scondList.add(new MyLine(tempLine.a, tempLine.b));
 		thirdList.add(new MyLine(tempLine.a, tempLine.b));
 	}
 
-	private void ww2(MyLine a) {
+	private void ww2(MyLine a, int wind) {
 
 		MyLine tempLine = new MyLine(a.a, a.b);
 
-		w2(tempLine.a);
-		w2(tempLine.b);
+		w2(tempLine.a, wind);
+		w2(tempLine.b, wind);
 
 		scondList.add(new MyLine(tempLine.a, tempLine.b));
 		thirdList.add(new MyLine(tempLine.a, tempLine.b));
 	}
 
-	private void ww3(MyLine a) {
+	private void ww3(MyLine a, int wind) {
 
 		MyLine tempLine = new MyLine(a.a, a.b);
 
-		w3(tempLine.a);
-		w3(tempLine.b);
+		w3(tempLine.a, wind);
+		w3(tempLine.b, wind);
 
 		scondList.add(new MyLine(tempLine.a, tempLine.b));
 		thirdList.add(new MyLine(tempLine.a, tempLine.b));
 	}
 
-	private void ww4(MyLine a) {
+	private void ww4(MyLine a, int wind) {
 
 		MyLine tempLine = new MyLine(a.a, a.b);
 
-		w4(tempLine.a);
-		w4(tempLine.b);
+		w4(tempLine.a, wind);
+		w4(tempLine.b, wind);
 
 		scondList.add(new MyLine(tempLine.a, tempLine.b));
 		thirdList.add(new MyLine(tempLine.a, tempLine.b));
 
 	}
 
-	// ---- utilities
-	private void decraseAngle() {
-		bp[0].angle = bp[0].angle + wind;
-		bp[1].angle = bp[0].angle + wind;
-		bp[2].angle = bp[0].angle + wind;
-		bp[3].angle = bp[0].angle + wind;
-	}
-	
-	
-
-	private void test() {
-
-		if (counter < 4) {
-			if (counter == 2) {
-				decraseAngle();
-			}
+	private void test(int wind) {
+		//scondList = new ArrayList<MyLine>();
+		scondList.add(new MyLine(250, 350, 250, 250));
+		firstList.add(new MyLine(250, 350, 250, 250));
+		
+		for (int i = 0; i < 4; i++) {
 			if (!firstList.isEmpty()) {
 				thirdList = new ArrayList<MyLine>();
 				while (!firstList.isEmpty()) {
@@ -239,49 +175,23 @@ public class TreeDrawingPanel extends JPanel {
 					else {
 						temp.setLocation(firstList.get(0).a);
 					}
-					ww1(firstList.get(0));
-					ww2(firstList.get(0));
-					ww3(firstList.get(0));
-					ww4(firstList.get(0));
+					// prideti vieja
+					ww1(firstList.get(0), wind);
+					ww2(firstList.get(0), wind);
+					ww3(firstList.get(0), wind);
+					ww4(firstList.get(0), wind);
 					firstList.remove(0);
 
 				}
 
 				firstList = thirdList;
 				thirdList = null;
-			} else {
-				
+
 			}
 			counter++;
-		}
-	}
-	
-	//
-	private void test2() {
-		if (counter < 4) {
-			if (!fourth.isEmpty()) {
-				sixth = new ArrayList<MyLine>();
-				while (!fourth.isEmpty()) {
-					if (counter % 2 == 0)
-						temp.setLocation(fourth.get(0).b);
-					else {
-						temp.setLocation(fourth.get(0).a);
-					}
-					ww21(fourth.get(0));
-					ww22(fourth.get(0));
-					ww23(fourth.get(0));
-					ww24(fourth.get(0));
-					fourth.remove(0);
 
-				}
-
-				fourth = sixth;
-				sixth = null;
-			} else {
-				
-			}
 		}
-		System.out.println(counter);
+		counter =0;
 	}
-	
+
 }
